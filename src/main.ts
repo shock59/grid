9a1754887f44d936f4eb911a32e03b3ca924cd8c
shock59 <42130427+shock59@users.noreply.github.com>
2025-11-07 15:59:10 +1100

    Move grid into class

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,39 @@
 import "./style.css";
 
+class Grid {
+  dimensions: { width: number; height: number };
+  element: HTMLDivElement;
+
+  constructor(dimensions: { width: number; height: number }) {
+    this.dimensions = dimensions;
+
+    this.element = document.createElement("div");
+    this.element.classList.add("grid");
+
+    for (let rowIndex = 0; rowIndex < this.dimensions.height; rowIndex++) {
+      const row = document.createElement("div");
+      row.classList.add("grid-row");
+
+      for (
+        let columnIndex = 0;
+        columnIndex < this.dimensions.width;
+        columnIndex++
+      ) {
+        const cell = document.createElement("div");
+        cell.classList.add("grid-cell");
+        cell.innerHTML = `${rowIndex * this.dimensions.width + columnIndex}`;
+        row.appendChild(cell);
+      }
+
+      this.element.appendChild(row);
+    }
+  }
+}
+
 // Create the grid
 const gridDimensions = {
   width: 16,
   height: 16,
 };
-const grid = document.createElement("div");
-grid.classList.add("grid");
-for (let rowIndex = 0; rowIndex < gridDimensions.height; rowIndex++) {
-  const row = document.createElement("div");
-  row.classList.add("grid-row");
-
-  for (let columnIndex = 0; columnIndex < gridDimensions.width; columnIndex++) {
-    const cell = document.createElement("div");
-    cell.classList.add("grid-cell");
-    cell.innerHTML = `${rowIndex * gridDimensions.width + columnIndex}`;
-    row.appendChild(cell);
-  }
-
-  grid.appendChild(row);
-}
-
-document.querySelector<HTMLDivElement>("#game")!.appendChild(grid);
+const grid = new Grid(gridDimensions);
+document.querySelector<HTMLDivElement>("#game")!.appendChild(grid.element);
