cc5b50874b1b32defe47af3f3f1eefa1c67291ca
shock59 <42130427+shock59@users.noreply.github.com>
2025-11-07 15:47:05 +1100

    Create a grid

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,24 @@
 import "./style.css";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = "Grid game";
+// Create the grid
+const gridDimensions = {
+  width: 16,
+  height: 16,
+};
+const grid = document.createElement("div");
+grid.classList.add("grid");
+for (let rowIndex = 0; rowIndex < gridDimensions.height; rowIndex++) {
+  const row = document.createElement("div");
+  row.classList.add("grid-row");
+
+  for (let columnIndex = 0; columnIndex < gridDimensions.width; columnIndex++) {
+    const cell = document.createElement("div");
+    cell.classList.add("grid-cell");
+    cell.innerHTML = `${rowIndex} ${columnIndex}`;
+    row.appendChild(cell);
+  }
+
+  grid.appendChild(row);
+}
+
+document.querySelector<HTMLDivElement>("#app")!.appendChild(grid);
