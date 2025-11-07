123f60a3892d72522189fa037aaf2f7535c49c6a
shock59 <42130427+shock59@users.noreply.github.com>
2025-11-07 16:17:42 +1100

    Add a sidebar element

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import "./style.css";
 
 class Grid {
   dimensions: { width: number; height: number };
+  containerElement: HTMLDivElement;
   element: HTMLDivElement;
 
   constructor(dimensions: { width: number; height: number }) {
     this.dimensions = dimensions;
 
+    this.containerElement = document.createElement("div");
     this.element = document.createElement("div");
+    this.containerElement.classList.add("grid-container");
     this.element.classList.add("grid");
 
     for (let rowIndex = 0; rowIndex < this.dimensions.height; rowIndex++) {
@@ -26,24 +29,40 @@ class Grid {
       }
 
       this.element.appendChild(row);
-
-      window.addEventListener("resize", () => this.updateCellSizes());
-      this.updateCellSizes();
     }
+    this.containerElement.appendChild(this.element);
+
+    window.addEventListener("resize", () => this.updateCellSizes());
+    this.updateCellSizes();
   }
 
   updateCellSizes() {
     const maxGridSize = Math.min(window.innerWidth, window.innerHeight);
     const cellCount = Math.max(this.dimensions.width, this.dimensions.height);
-    const cellSize = maxGridSize / (cellCount + 1); // Add 1 to cellCount to add a margin of half a cell
-    this.element.style.setProperty("--cell-size", `${cellSize}px`);
+    const cellSize = maxGridSize / (cellCount + 1 + 2); // Add 1 to cellCount to add a margin of half a cell and 2 for the sidebar
+    document
+      .querySelector<HTMLDivElement>("#game")!
+      .style.setProperty("--cell-size", `${cellSize}px`);
+    this.element.style.margin = `${cellSize / 2}px`;
   }
 }
 
-// Create the grid
+class Sidebar {
+  element: HTMLDivElement;
+
+  constructor() {
+    this.element = document.createElement("div");
+    this.element.classList.add("sidebar");
+  }
+}
+
 const gridDimensions = {
   width: 16,
   height: 16,
 };
 const grid = new Grid(gridDimensions);
-document.querySelector<HTMLDivElement>("#game")!.appendChild(grid.element);
+const sidebar = new Sidebar();
+
+const gameElement = document.querySelector<HTMLDivElement>("#game")!;
+gameElement.appendChild(grid.containerElement);
+gameElement.appendChild(sidebar.element);
